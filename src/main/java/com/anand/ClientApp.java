d1b304b07a5c4390a20fb15d55a2bcda5a8415e1
Anand <anandp@ilimi.in>
2019-03-12 02:42:45 +0530

    Modify the ClientApp class to invoke ModularClient

--- src/main/java/com/anand/ClientApp.java
+++ src/main/java/com/anand/ClientApp.java
@@ -1,13 +1,21 @@
 package com.anand;
 
 import com.anand.nonmodular.MonolithClient;
+import com.anand.testdriven.ModularClient;
+import okhttp3.OkHttpClient;
+
+import java.util.List;
 
 public class ClientApp {
 
+
     public static void main(String...args) {
-        MonolithClient monolithClient = new MonolithClient();
+        // MonolithClient monolithClient = new MonolithClient();
+        ModularClient modularClient = new ModularClient("http://localhost:9000/org/search/", new OkHttpClient());
         try {
-            monolithClient.searchOrg("0125134851644620800");
+            // monolithClient.searchOrg("0125134851644620800");
+            List<String> locationIds = modularClient.orgSearchApi("0125134851644620800");
+            System.out.println(String.join("," , locationIds));
         } catch (Exception ex) {
             System.out.println("Error occurred: " + ex.getMessage());
             ex.printStackTrace();
